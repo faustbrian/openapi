2fb385c8ab1107e61bae307b663d3e80018554c6
Brian Faust <hello@basecode.sh>
2023-03-13 12:56:40 +0200

    chore: apply global pint configuration

--- tests/Unit/Data/ExampleTest.php
+++ tests/Unit/Data/ExampleTest.php
@@ -9,7 +9,7 @@ use PreemStudio\OpenApi\Reader;
 
 it('can create an instance with mapped properties', function (): void {
     $reader = Reader::fromJsonFile('tests/Fixtures/api.github.com.json');
-    $data   = Example::from($reader->get('paths./.get.responses.200.content.application/json.examples.default'));
+    $data = Example::from($reader->get('paths./.get.responses.200.content.application/json.examples.default'));
 
     expect($data)->toBeInstanceOf(Example::class);
 });
